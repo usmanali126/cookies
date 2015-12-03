76abd6d0e5de775728b3391e5586fc2a4bfb3486
usmanali126 <usmanali126@hotmail.com>
2015-12-03 15:00:35 +0500

    final code
    this is final code for create favorite cookie data array using jquery
    and load only data with php that have in cookie array.

--- favorite.php
+++ favorite.php
@@ -1,8 +1,9 @@
 <?php
-if(isset($_COOKIE['favorite'])){
+if (isset($_COOKIE['favorite'])) {
     $cookie = explode(',', $_COOKIE['favorite']);
+    $fav = sizeof($cookie);
 }
-$fav=  sizeof($cookie);
+
 
 //exit();
 ?>
@@ -43,88 +44,117 @@ and open the template in the editor.
                 text-decoration: none;
                 cursor: pointer;
             }
+            .remove{
+                cursor: pointer;
+                margin-top: 10px;
+            }
         </style>
     </head>
     <body>
-        <a href="index.php">Index </a><span class="fav"> <?php echo isset($fav)? $fav :'';  ?></span>
-        <?php for ($i = 1; $i < 20; $i++) { 
-            if(in_array($i, $cookie)){?>
-        
-            <div class="cookie-div">
-                <span> cookie Number <?php echo $i; ?></span><span class="mark"  data-post-id="<?php echo $i; ?>"><?php echo $i; ?></span>
-            </div>
-            <?php
+        <a href="index.php">Index </a><sup class="fav"> <?php echo isset($fav) ? $fav : ''; ?></sup>
+        <?php
+        for ($i = 1; $i < 20; $i++) {
+            if (isset($cookie) && in_array($i, $cookie)) {
+                ?>
+
+                <div class="cookie-div">
+                    <span> cookie Number <?php echo $i; ?></span><span class="mark"  data-post-id="<?php echo $i; ?>"><?php echo $i; ?></span>
+                </div>
+                <?php
             }
         }
         ?>
+        <div><span class="remove">Remove All</span></div>
     </body>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script>
-        
-        var total;
-        $(document).ready(function () {
+
+        var favorite = 'favorite';
+        var exdays= 365;
+        var total = getCookie(favorite);
+        //alert(total);
+        if(total!=null){
+        var total = total.split(",");
+    }else{
+        total = new Array();
+        }
+//        $(document).ready(function () {
             //var postids= new Array();
-            var favorite='favorite';
-            $('.mark').on('click', function () {
+            
+            $('.cookie-div').on('click', '.mark', function () {
 //                alert('alert');
                 $(this).removeClass('mark');
                 $(this).addClass('cookie');
-                
-                var id=$(this).data("post-id");
-                id=id.toString()
-                var total = getCookie(favorite);
-                var total = total.split(",");
+
+                var id = $(this).data("post-id");
+                id = id.toString()
                 console.log(total);
                 //index=total.indexOf(id);
-                total.splice( $.inArray(id,total) ,1 );
+                total.splice($.inArray(id, total), 1);
                 console.log(total);
                 console.log(total.length);
                 $('.fav').html(total.length);
                 //postids.push(id);
                 //alert(name);
                 //console.log(postids);
-                setCookie(favorite, total);
+                if(total.length!=0){
+                  setCookie(favorite, total, exdays);  
+                }
+                else{
+                    setCookie(favorite, total, -2);
+                }
                 
+
             });
-            function setCookie(cname, cvalue) {
-                var d = new Date();
-                d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000));
-                var expires = "expires=" + d.toUTCString();
-                document.cookie = cname + "=" + cvalue + "; " + expires;
-                //alert('cookie set');
-            }
-            
-            function getCookie(name) {
-                var dc = document.cookie;
-                var prefix = name + "=";
-                var begin = dc.indexOf("; " + prefix);
-                if (begin == -1) {
-                    begin = dc.indexOf(prefix);
-                    if (begin != 0)
-                        return null;
-                }
-                else
-                {
-                    begin += 2;
-                    var end = document.cookie.indexOf(";", begin);
-                    if (end == -1) {
-                        end = dc.length;
-                    }
-                }
-                return unescape(dc.substring(begin + prefix.length, end));
-            }
+
+//        });
+
+        $('.remove').on('click',function(){
+           setCookie(favorite, total, -2);
+           $('.cookie-div').find('.mark').toggleClass('mark cookie');
+           $('.fav').html(0);
         });
-        $('.cookie-div').on('click','.cookie', function () {
+        
+        function setCookie(cname, cvalue, exdays) {
+            var d = new Date();
+            d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+            var expires = "expires=" + d.toUTCString();
+            document.cookie = cname + "=" + cvalue + "; " + expires;
+            //alert('cookie set');
+        }
+
+        function getCookie(name) {
+            var dc = document.cookie;
+            var prefix = name + "=";
+            var begin = dc.indexOf("; " + prefix);
+            if (begin == -1) {
+                begin = dc.indexOf(prefix);
+                if (begin != 0)
+                    return null;
+            }
+            else
+            {
+                begin += 2;
+                var end = document.cookie.indexOf(";", begin);
+                if (end == -1) {
+                    end = dc.length;
+                }
+            }
+            return unescape(dc.substring(begin + prefix.length, end));
+        }
+
+        $('.cookie-div').on('click', '.cookie', function () {
 //                alert('alert');
-               $(this).removeClass('cookie');
-               $(this).addClass('mark');
-                var id = $(this).data("post-id");
-                total.push(id);
-                //alert(name);
-                //console.log(postids);
-                setCookie(favorite, total);
-                //alert(total.length);
-                $('.fav').html(total.length);
-            });
+            $(this).removeClass('cookie');
+            $(this).addClass('mark');
+            var id = $(this).data("post-id");
+            total.push(id);
+            //alert(name);
+            //console.log(postids);
+            setCookie(favorite, total, exdays);
+            console.log(total);
+            //alert(total.length);
+            $('.fav').html(total.length);
+        });
     </script>
 </html>
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+if (isset($_COOKIE['favorite'])) {
+    $cookie = explode(',', $_COOKIE['favorite']);
+    $fav = sizeof($cookie);
+}
+
+
+//exit();
+?>
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -27,14 +36,19 @@ and open the template in the editor.
             }
             .mark{
                 background: green; 
+                padding: 5px;
+                margin-left: 5px;
+                border: 1px solid activecaption;
+                text-decoration: none;
+                cursor: pointer;
             }
         </style>
     </head>
     <body>
-        <a href="favorite.php">Favorites<span class="fav"></span></a>
+        <a href="favorite.php">Favorites<sup class="fav"> <?php echo isset($fav) ? $fav : ''; ?></sup></a>
         <?php for ($i = 1; $i < 20; $i++) { ?>
             <div class="cookie-div">
-                <span> cookie Number <?php echo $i; ?></span><span class="cookie"  data-post-id="<?php echo $i; ?>"><?php echo $i; ?></span>
+                <span> cookie Number <?php echo $i; ?></span><span class="<?php if(isset($cookie)){echo in_array($i, $cookie)?"mark":"cookie";}else{echo "cookie";} ?>"  data-post-id="<?php echo $i; ?>"><?php echo $i; ?></span>
             </div>
             <?php
         }
@@ -42,7 +56,92 @@ and open the template in the editor.
     </body>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script>
-        $(document).ready(function () {
+
+        var favorite = 'favorite';
+        var exdays= 365;
+        var total = getCookie(favorite);
+//        alert(total);
+        if(total!=null){
+        var total = total.split(",");
+        }else{
+        total = new Array();
+        }
+//        $(document).ready(function () {
+            //var postids= new Array();
+            
+            $('.cookie-div').on('click', '.mark', function () {
+//                alert('alert');
+                $(this).removeClass('mark');
+                $(this).addClass('cookie');
+
+                var id = $(this).data("post-id");
+                id = id.toString()
+                console.log(total);
+                //index=total.indexOf(id);
+                total.splice($.inArray(id, total), 1);
+                console.log(total);
+                console.log(total.length);
+                $('.fav').html(total.length);
+                //postids.push(id);
+                //alert(name);
+                //console.log(postids);
+                if(total.length!=0){
+                  setCookie(favorite, total, exdays);  
+                }
+                else{
+                    setCookie(favorite, favorite, -2);
+                }
+                
+
+            });
+
+//        });
+
+        
+        function setCookie(cname, cvalue, exdays) {
+            var d = new Date();
+            d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+            var expires = "expires=" + d.toUTCString();
+            document.cookie = cname + "=" + cvalue + "; " + expires;
+            //alert('cookie set');
+        }
+
+        function getCookie(name) {
+            var dc = document.cookie;
+            var prefix = name + "=";
+            var begin = dc.indexOf("; " + prefix);
+            if (begin == -1) {
+                begin = dc.indexOf(prefix);
+                if (begin != 0)
+                    return null;
+            }
+            else
+            {
+                begin += 2;
+                var end = document.cookie.indexOf(";", begin);
+                if (end == -1) {
+                    end = dc.length;
+                }
+            }
+            return unescape(dc.substring(begin + prefix.length, end));
+        }
+
+        $('.cookie-div').on('click', '.cookie', function () {
+//                alert('alert');
+            $(this).removeClass('cookie');
+            $(this).addClass('mark');
+            var id = $(this).data("post-id");
+            total.push(id);
+            //alert(name);
+            //console.log(postids);
+            setCookie(favorite, total, exdays);
+            console.log(total);
+            //alert(total.length);
+            $('.fav').html(total.length);
+        });
+    </script>
+    <script>
+        /*$(document).ready(function () {
             var postids = new Array();
             var favorite='favorite';
             $('.cookie').on('click', function () {
@@ -110,6 +209,6 @@ and open the template in the editor.
                 }
                 return unescape(dc.substring(begin + prefix.length, end));
             }
-        });
+        });*/
     </script>
 </html>
